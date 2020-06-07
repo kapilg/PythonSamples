8d2e1bfceccb84dd3a6835d4078d7c23b6cb4e82
Kapil Gupta <Kapil89gupta@gmail.com>
2020-06-07 21:49:18 +0530

    Added initial files

---
+++ Samples/Variable.py
@@ -0,0 +1,10 @@
+age = 20
+print(age) 
+sentence = "My name is John"
+print(sentence)
+name , age = "Mike",20
+print(name)
+print(age)
+test1 =test2 = test3 = 20
+print ('test1 number is', test1)
+print ("test1 number is {} ".format(test1))
